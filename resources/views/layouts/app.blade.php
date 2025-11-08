afe9611cc7fd45e69751d59c8e2a63dc5ef10288
adresz <tigerad97@gmail.com>
2025-11-08 16:31:38 +0100

    CSS frissítve a főoldalhoz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Laravel ZeroFour')</title>
+    <title>@yield('title', 'Magyar Utikalauz')</title>
+
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome 6 (ikonokhoz: nyilak, térkép, check, stb.) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+    <!-- Saját CSS (home.css, main.css, stb.) -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
-    
+
     @stack('styles')
 </head>
 <body class="@yield('body-class', 'is-preload')">
@@ -18,6 +26,7 @@
 
     @include('partials.footer')
 
+    <!-- jQuery + saját JS-ek (ha használod) -->
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/jquery.dropotron.min.js') }}"></script>
     <script src="{{ asset('js/browser.min.js') }}"></script>
@@ -25,8 +34,9 @@
     <script src="{{ asset('js/util.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
 
+    <!-- Bootstrap 5 JS (carouselhez kötelező!) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @stack('scripts')
-
 </body>
 </html>
